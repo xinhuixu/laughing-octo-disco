7b499dbcb90dc1b61ab3d3af1b5042c43e8b614a
goyyapazham <nalanda99@gmail.com>
2017-01-19 12:22:24 -0500

    cleaner file reader but access to array elements still unclear

--- utils.c
+++ utils.c
@@ -6,35 +6,45 @@
 #include <sys/uio.h>
 #include <fcntl.h>
 
+//ty stackoverflow: http://stackoverflow.com/questions/20013693/read-csv-file-to-a-2d-array-on-c
+
 int main() {
 
   FILE *fd = fopen("test.txt", "r");
-  char buf[256];
-  char*** lines = (char***)malloc(50);
-  int j=0;
-  
-  while( fgets(buf, 256, fd) ) {
-    char** line = (char**)malloc(50);
-    int i = 0;
-    char *x = buf;
-    while((line[i] = strsep(&x, ","))) i++;
-    i=0;
-    printf("");
-    while(line[i]) {
-      printf("%s ", line[i]); i++;
-    }
-    lines[j] = line;
-    j++;
-  }
+  char buf[1024];
+  char *line, *record;
+  int i=0, j=0;
+  int arr[10][10];
 
-  int i=0;
-  while(lines[i]) {
-    printf("line[0]%s\n", lines[i][0]);
-    
-    //printf("[%s, %s, %s]\n", line[0], line[1], line[2]);
-    i++;
+  for(i=0; i<10; i++) {
+    for(j=0; j<10; j++) {
+      arr[i][j] = 0;
+    }
+  }
+  
+  if( fd == NULL ) {
+    printf("Couldn't open file!\n");
+    return -1;
+  }
+  while( (line = fgets(buf, sizeof(buf), fd)) != NULL ) {
+    record = strtok(line, ",");
+    while( record != NULL ) {
+      //printf("record: %s\n", record);
+      arr[i][j++] = atoi(record);
+      record = strtok(NULL, ",");
+    }
+    ++i;
   }
 
   fclose(fd);
+
+  for(i=0; i<10; i++) {
+    printf("[");
+    for(j=0; j<10; j++) {
+      printf("  %d", arr[i][j]);
+    }
+    printf("  ]\n");
+  }
+
   return 0;
 }
