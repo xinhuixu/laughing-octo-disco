d194a062d28ab80f3a312a32ba7168709c2f94a2
goyyapazham <nalanda99@gmail.com>
2017-01-19 15:34:28 -0500

    commented utils.c

--- utils.c
+++ utils.c
@@ -10,13 +10,15 @@
 //ty stackoverflow: http://stackoverflow.com/questions/20013693/read-csv-file-to-a-2d-array-on-c
 
 int main() {
-  FILE *fd = fopen("test.txt", "r");
-  char buf[1024];
-  char *line, *record;
-  int i=0, j=0, k=0;
-  int rows=3, cols=0;
-  char arr[10][3][1024];
+  
+  FILE *fd = fopen("test.txt", "r"); //open file
+  char buf[1024]; //file will be read into this
+  char *line, *record; //for reading into array purposes
+  int i=0, j=0, k=0; //counters
+  int rows=3, cols=0; //for printing purposes
+  char arr[10][3][1024]; //array!
 
+  //just in case data is missing, etc
   char *add = "none";
   for(i=0; i<10; i++) {
     for(j=0; j<3; j++) {
@@ -26,19 +28,25 @@ int main() {
       add = "none";
     }
   }
-  
-  i=0; j=0;
+
+  i=0; j=0; //reset counters
+  //error handling for file opening
   if( fd == NULL ) {
     printf("Couldn't open file!\n");
     return -1;
   }
+  //read file line-by-line
   while( (line = fgets(buf, sizeof(buf), fd)) != NULL ) {
+    //split line by commas, strip newline
     record = strtok(strtok(line, "\n"), ",");
+    //"iterate" thru values in record
     while( record != NULL ) {
+      //and copy value char-by-char into the array
       add = record;
       for(k=0; k<strlen(record)+1; k++, add++) {
 	strcpy(&arr[i][j][k], add);
       }
+      //move to next value in record
       record = strtok(NULL, ",");
       j++;
     }
@@ -46,7 +54,8 @@ int main() {
     i++;
     cols++;
   }
-  
+
+  //printy things
   for(i=0; i<cols; i++) {
     printf("[");
     for(j=0; j<rows; j++) {
@@ -54,7 +63,8 @@ int main() {
     }
     printf("  ]\n");
   }
-  
+
+  //close file
   fclose(fd);
 
   return 0;
