5a82871636dd31e4fe59097b3963f1f3385385fd
goyyapazham <nalanda99@gmail.com>
2017-01-19 15:31:58 -0500

    read csv into 2d array works :)

--- utils.c
+++ utils.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdarg.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <fcntl.h>
@@ -9,43 +10,52 @@
 //ty stackoverflow: http://stackoverflow.com/questions/20013693/read-csv-file-to-a-2d-array-on-c
 
 int main() {
-
   FILE *fd = fopen("test.txt", "r");
   char buf[1024];
   char *line, *record;
-  int i=0, j=0;
-  char arr[10][10];
+  int i=0, j=0, k=0;
+  int rows=3, cols=0;
+  char arr[10][3][1024];
 
+  char *add = "none";
   for(i=0; i<10; i++) {
-    for(j=0; j<10; j++) {
-      arr[i][j] = 0;
+    for(j=0; j<3; j++) {
+      for(k=0; k<strlen("none")+1; k++, add++) {
+	strcpy(&arr[i][j][k], add);
+      }
+      add = "none";
     }
   }
   
+  i=0; j=0;
   if( fd == NULL ) {
     printf("Couldn't open file!\n");
     return -1;
   }
   while( (line = fgets(buf, sizeof(buf), fd)) != NULL ) {
-    record = strtok(line, ",");
+    record = strtok(strtok(line, "\n"), ",");
     while( record != NULL ) {
-      printf("record: %s\n", record);
-      arr[i][j++] = *record;
-      printf("arr[i][j]: %s\n", arr[i][j]);
+      add = record;
+      for(k=0; k<strlen(record)+1; k++, add++) {
+	strcpy(&arr[i][j][k], add);
+      }
       record = strtok(NULL, ",");
+      j++;
     }
-    ++i;
+    j=0;
+    i++;
+    cols++;
   }
-
-  fclose(fd);
-  /*
-  for(i=0; i<10; i++) {
+  
+  for(i=0; i<cols; i++) {
     printf("[");
-    for(j=0; j<10; j++) {
-      printf("  %d", arr[i][j]);
+    for(j=0; j<rows; j++) {
+      printf("  %s", arr[i][j]);
     }
     printf("  ]\n");
   }
-  */
+  
+  fclose(fd);
+
   return 0;
 }
