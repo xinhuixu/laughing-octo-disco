4a15102ec0c56824c3533931b9af2e3cf2a9b7e4
goyyapazham <nalanda99@gmail.com>
2017-01-23 16:10:09 -0500

    deleting everything i did last night lol

--- func.c
+++ func.c
@@ -223,53 +223,11 @@ bool is_manager( char* username, char* proj_name ) {
 
 void all_tasks( char* buffer, char* username );
 void my_tasks( char* buffer, char* username );
-void add_task( char* buffer, char* proj_name, char* username ) {
-
-  char path[100];
-  sprintf(path, "projects/%s/%s/tasks.csv", username, proj_name);
-  char arr[100][4][1024];
-  int r = parse_csv(path, arr);
-
-  //what is *buffer?
-  /*
-  int a = add_row(path, arr, username, [task], [deadline], [status], r, 4);
-  if( a == r+1 )
-    printf("Success!\n");
-  else
-    printf("Something went wrong.\n");
-  */
-  
-}
-void remove_task( char* proj_name, char* username, char* buffer ) {
-
-  char path[100];
-  sprintf(path, "projects/%s/%s/tasks.csv", username, proj_name);
-  char arr[100][4][1024];
-  int r = parse_csv(path, arr);
-  int rem = remove_row(path, arr, username, buffer, r, 4);
-  if( rem == r-1 )
-    printf("Success!\n");
-  else
-    printf("Something went wrong.\n");
-
-}
-void remove_member( char* to_rem ) {
-
-  char path[100];
-  sprintf(path, "projects/%s/%s/members.csv", to_rem, proj_name);
-  char arr[100][4][1024];
-  int r = parse_csv(path, arr);
-  int rem = remove_row(path, arr, to_rem, "", r, 1);
-  if( rem == r-1 )
-    printf("Success!\n");
-  else
-    printf("Something went wrong.\n");
-  
-}
+void add_task( char* buffer, char* proj_name, char* username );
+void remove_task( char* proj_name, char* username, char* buffer );
+void remove_member( char* to_rem );
 void add_member( char* new_member );
-
-void mark( char* buffer );
-
+void update_status( char* buffer );
 
 /* borrowed from http://www.csl.mtu.edu/cs4411.ck/www/NOTES/process/fork/exec.html */
 /* lmao were not using this probably*/
