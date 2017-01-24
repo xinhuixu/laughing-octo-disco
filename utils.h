3b42e546f6e42b6b8f7fdb78221699d1be72e67e
goyyapazham <nalanda99@gmail.com>
2017-01-24 01:38:08 -0500

    did a thing

--- utils.h
+++ utils.h
@@ -4,8 +4,7 @@
 int parse_csv(char *filename, char arr[100][4][1024]);
 int add_row(char *filename, char arr[100][4][1024], char *uname, char *task, char *dline, char *stat, int rows, int cols);
 int add_helper(char *filename, char add[4][1024], char arr[100][4][1024], int rows, int cols);
-int edit_dline(char arr[100][4][1024], char *uname, char *task, char *newdline, int rows, int cols);
-int edit_status(char arr[100][4][1024], char *uname, char *task, char *newstat, int rows, int cols);
+int edit_status(char *filename, char arr[100][4][1024], char *uname, char *task, char *newstat, int rows, int cols);
 int remove_row(char *filename, char arr[100][4][1024], char *uname, char *task, int rows, int cols);
 void print_arr(char arr[1024][4][1024], int rows, int cols);
 void print_file(char *filename, int rows, int cols);
